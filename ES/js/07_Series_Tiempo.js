27a05f44ae2de9749d2478ee75653377c3b0bf43
David Montero Loaiza <49817852+davemlz@users.noreply.github.com>
2020-03-15 17:22:55 +0100

    Path

--- ES/js/07_Series_Tiempo.js
+++ ES/js/07_Series_Tiempo.js
@@ -1,67 +1,67 @@
 // ********************************************************************************
 // SERIES DE TIEMPO
 // ********************************************************************************
-// Las series de tiempo se construyen en gr·ficos que pueden exportarse como tal o como tablas
-// Es necesario una colecciÛn de im·genes y una region o series de regiones
-// Se utiliza la funciÛn ui.Chart.image.series("ColecciÛn","Shape","Reductor","ResoluciÛn")
+// Las series de tiempo se construyen en gr√°ficos que pueden exportarse como tal o como tablas
+// Es necesario una colecci√≥n de im√°genes y una region o series de regiones
+// Se utiliza la funci√≥n ui.Chart.image.series("Colecci√≥n","Shape","Reductor","Resoluci√≥n")
 // ********************************************************************************
 
 // Traemos un shape
-var shape = ee.FeatureCollection("users/dmlmont/Taller_GEE_Univalle/SHP");
+var shape = ee.FeatureCollection("path/SHP");
 
 // Se imprime el shape en la consola para ver sus columnas
-// En Series de tiempo por regiÛn es necesario
+// En Series de tiempo por regi√≥n es necesario
 print("SHAPEFILE",shape);
 
-// Traemos una colecciÛn de Sentinel-2 filtrada por fecha, ubicaciÛn y metadatos
+// Traemos una colecci√≥n de Sentinel-2 filtrada por fecha, ubicaci√≥n y metadatos
 var s2 = ee.ImageCollection("COPERNICUS/S2")
   .filterDate("2018-01-01","2018-12-31")
   .filterBounds(shape)
   .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',20));
 
-// Vamos a calcular el NDVI para toda la colecciÛn
-// Para poder hacerlo hay que crear una funciÛn
-// Se empieza llamando la clase "function", el nombre de la funciÛn y los argumentos
-// En este caso el nombre de la funciÛn es "NDVI" y el argumento es "img"
+// Vamos a calcular el NDVI para toda la colecci√≥n
+// Para poder hacerlo hay que crear una funci√≥n
+// Se empieza llamando la clase "function", el nombre de la funci√≥n y los argumentos
+// En este caso el nombre de la funci√≥n es "NDVI" y el argumento es "img"
 // Estos dos pueden tener los nombres que se deseen
-// El argumento "img" representar· cada imagen de la colecciÛn
-// Dentro de la funciÛn podemos crear m·s variables con la clase "var"
-// Se crea dentro una variable "image" que ser· el NDVI de la imagen, recortada.
-// La funciÛn .clip("shape") corta la imagen con el shape deseado
-// La funciÛn .normalizedDifference("Bandas") calcula el NDVI de la imagen recortada
-// La funciÛn "return" retorna la variable "image"
-// La funciÛn .copyProperties() es obligatoria para mantener las fechas de las im·genes
+// El argumento "img" representar√° cada imagen de la colecci√≥n
+// Dentro de la funci√≥n podemos crear m√°s variables con la clase "var"
+// Se crea dentro una variable "image" que ser√° el NDVI de la imagen, recortada.
+// La funci√≥n .clip("shape") corta la imagen con el shape deseado
+// La funci√≥n .normalizedDifference("Bandas") calcula el NDVI de la imagen recortada
+// La funci√≥n "return" retorna la variable "image"
+// La funci√≥n .copyProperties() es obligatoria para mantener las fechas de las im√°genes
 function NDVI(img){
   var image = img.clip(shape).normalizedDifference(['B8','B2']);
   return image.copyProperties(img, ['system:time_start']);
 }
 
-// La funciÛn .map("FunciÛn") se usa para aplicar una funciÛn sobre cada imagen de una colecciÛn
-// En este caso se aplica la funciÛn NDVI sobre la colecciÛn de Sentinel-2
+// La funci√≥n .map("Funci√≥n") se usa para aplicar una funci√≥n sobre cada imagen de una colecci√≥n
+// En este caso se aplica la funci√≥n NDVI sobre la colecci√≥n de Sentinel-2
 var s2_NDVI = s2.map(NDVI);
 
 // Se crea la serie de tiempo del NDVI
 var ts = ui.Chart.image.series(s2_NDVI,shape,ee.Reducer.mean(),10);
 
 // Se imprime la serie de tiempo en la consola
-// Es un gr·fico
+// Es un gr√°fico
 print(ts);
 
 // ********************************************************************************
-// SERIES DE TIEMPO POR REGI”N
+// SERIES DE TIEMPO POR REGI√ìN
 // ********************************************************************************
-// Para las series de tiempo por regiÛn se necesitan otros par·metros
-// Se utiliza la funciÛn ui.Chart.image.seriesByRegion("ColecciÛn","Shape","Reductor","Nombre de la banda","ResoluciÛn","Propiedad de tiempo","Atributo del shape")
-// El atributo del shape es importante ya que ser· el divisor de las regiones
-// Si no se ponde el atributo del shape se har· por defecto la divisiÛn con el id otorgado por GEE
+// Para las series de tiempo por regi√≥n se necesitan otros par√°metros
+// Se utiliza la funci√≥n ui.Chart.image.seriesByRegion("Colecci√≥n","Shape","Reductor","Nombre de la banda","Resoluci√≥n","Propiedad de tiempo","Atributo del shape")
+// El atributo del shape es importante ya que ser√° el divisor de las regiones
+// Si no se ponde el atributo del shape se har√° por defecto la divisi√≥n con el id otorgado por GEE
 // ********************************************************************************
 
 // Se crea la serie de tiempo por regiones
 // El reductor es la media: ee.Reducer.mean()
 // El nombre de la banda del NDVI es "nd"
-// Si no saben el nombre da la banda pueden saberlo con la funciÛn .bandNames()
-// "system:time_start" es la propiedad de tiempo de la colecciÛn
-// "COD_UNICO" es el atributo del shape elegido para realizar la divisiÛn
+// Si no saben el nombre da la banda pueden saberlo con la funci√≥n .bandNames()
+// "system:time_start" es la propiedad de tiempo de la colecci√≥n
+// "COD_UNICO" es el atributo del shape elegido para realizar la divisi√≥n
 var ts_regions = ui.Chart.image.seriesByRegion(
   s2_NDVI,
   shape,
@@ -71,6 +71,6 @@ var ts_regions = ui.Chart.image.seriesByRegion(
   'system:time_start',
   'Lote');
 
-// Se imprime la serie de tiempo por regiÛn en la consola
-// Es un gr·fico
+// Se imprime la serie de tiempo por regi√≥n en la consola
+// Es un gr√°fico
 print(ts_regions);
