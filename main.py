f639bf1e425ac563356b0a4c17c727375eb137de
David Montero Loaiza <49817852+davemlz@users.noreply.github.com>
2022-05-08 23:25:46 +0200

    Updated README

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ def filterByTalkType():
 
 letters = list(map(chr, range(65, 91)))
 
+flags = {
+    "en": '<img src="https://raw.githubusercontent.com/davemlz/outreach/master/_static/united-kingdom.png" alt="EN" height="30">'
+    "es": '<img src="https://raw.githubusercontent.com/davemlz/outreach/master/_static/spain.png" alt="ES" height="30">'
+}
+
 text = []
 for talkType in ["talk","lightning talk","workshop","poster"]:
     text.append(f"\n## {talkType.capitalize()}\n\n<table>")
@@ -25,8 +30,9 @@ for talkType in ["talk","lightning talk","workshop","poster"]:
         if attributes['event'] == talkType:
             link = f"https://github.com/davemlz/outreach/tree/master/{attributes['language'].upper()}/{attributes['folder']}"
             name = attributes['name']
-            line = f'<tr><td width="50%"><a href="{link}" target="_blank">{name}</a></td></tr>\n'
-            text.append(line)
+            text.append(f'<tr><td width="50%"><a href="{link}" target="_blank">{name}</a></td>')
+            text.append(f'<td width="50%">{flags[attributes["language"]]}</td>')
+            text.append('</tr>\n')
     text.append("</table>\n")
 
 with open('README.md', 'w') as f:
